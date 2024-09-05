43abc64af9185c2df052e5e778a26e35a9552faf
ethbra <brassfield.ethan@ic.edu>
2024-09-05 16:05:31 -0500

    init

---
+++ src/test/java/com/budgetking/budgetking/BudgetKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetking.budgetking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetKingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
